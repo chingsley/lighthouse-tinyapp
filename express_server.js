c468494e24c2ae4a86466224b8e56bc1652c5570
Kingsley Eneja <eneja.kc@gmail.com>
2021-12-02 16:53:04 +0100

    - Get by shortURL value

--- express_server.js
+++ express_server.js
@@ -17,6 +17,12 @@ app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
+app.get("/urls/:shortURL", (req, res) => {
+  const { shortURL } = req.params;
+
+  const templateVars = { shortURL, longURL: urlDatabase[shortURL] };
+  res.render("urls_show", templateVars);
+});
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
